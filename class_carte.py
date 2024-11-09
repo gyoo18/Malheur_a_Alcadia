fdda27e80f83fb6e25ea03744665e210d02587f9
Frankydud3 <159861466+Frankydud3@users.noreply.github.com>
2024-11-09 14:18:10 -0500

    Add files via upload

---
+++ class_carte.py
@@ -0,0 +1,26 @@
+class Carte:
+    def __init__(self,ligne : int ,colonne :int):
+        self.ligne = ligne
+        self.colonne= colonne
+
+    
+
+    def creation(self):
+        matrice = []
+        for ligne in range(self.ligne):
+            ligne_donnee = []
+            for colonne in range(self.colonne):
+                ligne_donnee.append("0")
+            matrice.append(ligne_donnee)
+        return matrice
+    
+            
+    
+
+carte_1 = Carte(10,10) 
+
+mape = carte_1.creation()
+print(mape)
+
+for ligne,colonne in enumerate(mape):
+    print(ligne,colonne)
