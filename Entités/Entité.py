8570072cd6316bb8722bc63bf1293cd3bda9707f
gyoo18 <59801827+gyoo18@users.noreply.github.com>
2024-11-15 10:44:44 -0500

    Ajouté système d'attaque et défense de base à entité

--- Entités/Entité.py
+++ Entités/Entité.py
@@ -1,6 +1,7 @@
 from typing_extensions import Self
 import sys
 from Maths.Vec2 import Vec2
+from class_carte import Carte
 
 class État:
     RECHERCHE = "recherche"
@@ -10,7 +11,7 @@ class État:
 
     v : str
 
-    def __init__(self, valeur = Self.RECHERCHE):
+    def __init__(self, valeur = RECHERCHE):
         self.v = valeur
 
 class ÉtatCombat:
@@ -20,23 +21,30 @@ class ÉtatCombat:
 
     v : str
 
-    def __init__(self, valeur = Self.LIBRE):
+    def __init__(self, valeur = LIBRE):
         self.v = valeur
 
 class Attaque:
 
+    dégats : float
+
     def __init__(self):
-        pass
+        self.dégats = 0
 
 class Entité:
 
     estVivant : bool
+    vie : float
 
     état : État
     étatCombat : ÉtatCombat
     ennemi : Self
     pos : Vec2
     destination : Vec2
+
+    dégats_défense : float
+    dégats_libre : float
+    dégats_charger : float
 
     chemin : list[Tuile] # TODO implémenter Tuiles
 
@@ -47,10 +55,14 @@ class Entité:
         self.ennemi = None
         self.pos = Vec2(0,0)
         self.destination = Vec2(0,0)
+        self.dégats_défense = 0.5
+        self.dégats_libre = 1.0
+        self.dégats_charger = 1.5
+        self.vie = 100.0
         pass
 
     def MiseÀJourIA(self):
-        carte = Carte() # TODO implémenter carte
+        carte = Carte()
         if self.état.v == État.RECHERCHE:
             ennemiPlusPrès = None
             distanceMinimale = sys.float_info.max
@@ -93,13 +105,29 @@ class Entité:
         pass
     
     def AttaquerEnnemi(self):
-        pass    # TODO implémenter Entitié.AttaquerEnnemi()
+        attaque = Attaque()
+        attaque.dégats = 1.0
+        self.ennemi.Attaquer(attaque)
 
     def Attaquer(self, attaque : Attaque):
-        pass    # TODO implémenter Entitié.Attaquer()
+        attaque = self.Défense(attaque)
+        self.vie -= attaque.dégats
+        if self.vie <= 0.0:
+            self.vie = 0.0
+            self.estVivant = False
 
     def Défense(self, attaque : Attaque):
-        return attaque  # TODO implémenter Entitié.Défense()
+        match self.état.v:
+            case ÉtatCombat.DÉFENSE:
+                attaque.dégats *= self.dégats_défense
+            case ÉtatCombat.LIBRE:
+                attaque.dégats *= self.dégats_libre
+            case ÉtatCombat.CHARGER:
+                attaque.dégats *= self.dégats_charger
+            case _:
+                raise ValueError("[Éntité.Défense()] L'état de combat " + self.étatCombat.v + " n'est pas un état valide.")
+
+        return attaque
 
     def A_étoile(self, carte : Carte, départ : Vec2, arrivé : Vec2):
         pass    # TODO implémenter Entitié.A_étoile()
