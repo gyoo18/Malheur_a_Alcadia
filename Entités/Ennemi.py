f090b68d3f21694049ca306189348c3c78d38fb7
Dreamod4 <lahaiesamuel05@gmail.com>
2024-11-20 09:06:10 -0500

    MÀJ

--- Entités/Ennemi.py
+++ Entités/Ennemi.py
@@ -1,6 +1,40 @@
 from Entités.Entité import *
+import random
+def Random_Stats(x,y):
+    Stats=int(random.choice(range(x,y)))
+    return Stats
 
 class Ennemi(Entité):
 
     def __init__(self):
         super().__init__()
+
+class Child_1(Ennemi):
+    def Stats_Child_1():
+        HP=50
+        ATT=Random_Stats(8,15)
+        DEF=Random_Stats(5,11)
+
+class Miners(Ennemi):
+    def Stats_Miners():
+        HP=75
+        ATT=Random_Stats(14,21)
+        DEF=Random_Stats(10,16)
+
+class Child_2(Ennemi):
+    def Stats_Child_2():
+        HP=75
+        ATT=Random_Stats(12,17)
+        DEF=Random_Stats(12,15)
+
+class Knight(Ennemi):
+    def Stats_Knight():
+        HP=125
+        ATT=Random_Stats(19,26)
+        DEF=Random_Stats(30,36)
+
+class Gunner(Ennemi):
+    def Stats_Gunner():
+        HP=50
+        ATT=Random_Stats(30,33)
+        DEF=Random_Stats(5,11)
--- Entités/Golem.py
+++ Entités/Golem.py
@@ -1,4 +1,5 @@
 from Entités.Entité import *
+import random
 
 class Commande:
     def __init__():
@@ -11,3 +12,25 @@ class Golem(Entité):
 
     def commande(self, commande : Commande):
         pass
+
+class Earth_golem(Golem):
+    def __init__(self):
+        super().__init__()
+
+    def Stats_Earth_golem():
+         HP=150
+         ATT=int(random.choice(range(10,16)))
+         DEF=int(random.choice(range(39,46)))
+         
+class water_golems(Golem):
+    def Stats_water_golem():
+        HP=90
+        ATT=int(random.choice(range(20,26)))
+        DEF=int(random.choice(range(25,31)))
+
+class Fire_Golem(Golem):
+     def Stats_Fire_golem():
+         HP=120
+         ATT=int(random.choice(range(26,29)))
+         DEF=int(random.choice(range(35,39)))
+    
