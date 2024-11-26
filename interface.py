0c9ad18c16de5f565f03eee7ac6472dc363e908b
Vincent Lafleur <lafv37@uqo.ca>
2024-11-26 10:43:42 -0500

    Update interface.py
    ceci est un exemple d'interface, il suffirait d'insserer la map et les controles.

--- interface.py
+++ interface.py
@@ -1,1 +1,44 @@
+#exemple de ui possible
+
+import os
+import time
+
+
+def clearScreen():
+    os.system("cls" if os.name == 'nt' else CLEAR)
+
+def dysplayUI(game_map):
+    clearScreen()
+
+    header = "=" * 50
+    header2 = "inserez titre".center(50)
+    header3 = "=" * 50
+
+    print(header)
+    print(header2)
+    print(header3)
+
+
+
+
+    footer = "=" * 50
+    controls = "w (up) s (down) a (left) d (right) q (quit)"
+    footer_end = "=" * 50
+
+    print(footer)
+    print(controls)
+    print(footer_end)
+
+
+def main():
+    width, eight = 20, 10
+
+    game_map = [["." for _ in range(width)] for _ in range(eight)]
+
+    while True:
+        dysplayUI(game_map)
+        break
+
+if __name__ == "__main__":
+    main()
 
